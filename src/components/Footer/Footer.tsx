bb5e4dcf1c9d3ac51e74e3666ee592c3ff107d50
Dmytro Haidash <tipocto.ckujiji@gmail.com>
2024-04-17 20:54:59 +0300

    implement footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,1 +1,35 @@
-export const Footer = () => <div>Footer</div>;
+import React from "react";
+import { Link } from "react-router-dom"
+import styles from "./Footer.module.scss"
+
+export const Footer = () => {
+  console.log(styles);
+
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footer__wrapper}>
+        <div className={styles.footer__logo}></div>
+
+        <ul className={styles.footer__list}>
+          <li className={styles.footer__item}>
+            <Link to="#" className={styles.footer__link}>Github</Link>
+          </li>
+
+          <li className={styles.footer__item}>
+            <Link to="#" className={styles.footer__link}>Contacts</Link>
+          </li>
+
+          <li className={styles.footer__item}>
+            <Link to="#" className={styles.footer__link}>Rights</Link>
+          </li>
+        </ul>
+
+        <div className={styles['back-to-top']}>
+          <span className={styles['back-to-top__text']}>Back to top</span>
+
+          <button className={styles['back-to-top__button']}></button>
+        </div>
+      </div>
+    </footer>
+  );
+};
