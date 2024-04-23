0de688c0265b21e2108be8cb26558c0f57b7eaf0
Yaroslav Horodynskyi <gorodinskiyyar@gmail.com>
2024-04-23 16:23:42 +0300

    add product slider

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Slider } from '../../components/Slider/Slider';
+import { ProductsSlider } from '../../components/ProductsSlider/ProductsSlider';
 export const HomePage = () => {
   return (
     <>
       <h1 className="title">Welcome to Nice Gadgets store!</h1>
       <Slider/>
+      <ProductsSlider title="Brand new models" />
     </>
   );
 };
