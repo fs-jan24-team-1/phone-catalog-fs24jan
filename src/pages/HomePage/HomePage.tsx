7b5028a5fbc4017de3300147d65a5fbd17b43b4f
Mariia Dzhafarova <grinchak.1997@gmail.com>
2024-04-23 19:08:34 +0300

    feat add category section

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Slider } from '../../components/Slider/Slider';
+import { CategoriesSection } from '../../components/CategoriesSection';
 export const HomePage = () => {
   return (
     <>
       <h1 className="title">Welcome to Nice Gadgets store!</h1>
-      <Slider/>
+      <Slider />
+      <CategoriesSection />
     </>
   );
 };
