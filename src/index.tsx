6671456c30c8f119cb68387973aed8343b3bf126
Oleh Duma <hakerprof@gmail.com>
2024-04-24 22:59:18 +0300

    add language switch logic and buttons

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,29 @@ import { createRoot } from 'react-dom/client';
 import { Root } from './Root';
 import { Provider } from 'react-redux';
 import store from './store/store';
+import i18next from 'i18next';
+
+import global_en from './translations/en/global.json';
+import global_ua from './translations/ua/global.json';
+import { I18nextProvider } from 'react-i18next';
+
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: 'en',
+  resources: {
+    en: {
+      global: global_en,
+    },
+    ua: {
+      global: global_ua,
+    },
+  },
+});
 
 createRoot(document.getElementById('root') as HTMLDivElement).render(
-  <Provider store={store}>
-    <Root />
-  </Provider>,
+  <I18nextProvider i18n={i18next}>
+    <Provider store={store}>
+      <Root />
+    </Provider>
+  </I18nextProvider>,
 );
