bf7c78e3c8c8dbfd8169c94724d2b18a9efc518d
Mariia Dzhafarova <grinchak.1997@gmail.com>
2024-04-24 12:41:25 +0300

    feat add cart modal

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -4,13 +4,13 @@ import styles from './cartPage.module.scss';
 import { CartItem } from '../../components/CartIem';
 import { ButtonPrimary } from '../../components/UI/ButtonPrimary';
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { ButtonBack } from '../../components/UI/ButtonBack';
 import { LottieAnimation } from '../../components/UI/LottieAnimation';
 import * as animationData from './../../animations/EmptyCart.json';
 import { Link } from 'react-router-dom';
 import { ProductButtonType } from '../../types/ProductButtonType';
-
+import { CartModal } from '../../components/CartModal';
 
 export const CartPage = () => {
   const { cart, cartTotalAmount, cartTotalQuantity } = useSelector(
@@ -22,9 +22,14 @@ export const CartPage = () => {
     dispatch({ type: 'product/getTotals' });
   }, [cart, dispatch]);
 
-  //Need to DELETE or RECREATE this temp function
-  const tempFunction = () => {
-    return 0;
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
   };
 
   return (
@@ -35,13 +40,12 @@ export const CartPage = () => {
 
       {cartTotalQuantity === 0 ? (
         <div className={styles.container__empty__cart}>
-          <h1 style={{textAlign: 'center'}}>{`Your cart is empty :(`}</h1>
+          <h1 style={{ textAlign: 'center' }}>{`Your cart is empty :(`}</h1>
           <LottieAnimation animationData={animationData} />
           <Link to="/" className={styles.button}>
-          Сontinue shopping
-      </Link>
+            Сontinue shopping
+          </Link>
         </div>
-
       ) : (
         <div className={styles.wrapper}>
           <div className={styles.wrapper__products}>
@@ -61,11 +65,13 @@ export const CartPage = () => {
             <div className={styles.totalCost__line}></div>
             <ButtonPrimary
               textForPrimaryButton={ProductButtonType.CHECKOUT}
-              callback={tempFunction}
+              callback={openModal}
             />
           </div>
         </div>
       )}
+
+      <CartModal isOpen={modalIsOpen} onClose={closeModal} />
     </div>
   );
 };
