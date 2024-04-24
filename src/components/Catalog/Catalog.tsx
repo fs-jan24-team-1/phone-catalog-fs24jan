9d91f78a9658f8936fd370d2369ed4829855b319
Oleh Duma <hakerprof@gmail.com>
2024-04-24 18:35:03 +0300

    add loader, delay, and catalog loading

--- src/components/Catalog/Catalog.tsx
+++ src/components/Catalog/Catalog.tsx
@@ -5,6 +5,8 @@ import { Filter } from '../Filter/Filter';
 import { LottieAnimation } from '../UI/LottieAnimation';
 import * as animationData from '../../animations/ProductsNotFound.json';
 import { Link, useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Loader } from '../Loader';
 
 interface Props {
   products: Product[];
@@ -13,10 +15,16 @@ interface Props {
 
 export const Catalog: React.FC<Props> = ({ products, totalProducts }) => {
   const { pathname } = useLocation();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setIsLoading(false), 1000);
+  }, [products, pathname]);
 
   return (
     <section className={styles.section}>
-      {products.length > 0 ? (
+      {isLoading && <Loader />}
+      {!isLoading && products.length > 0 ? (
         <div className={styles.container}>
           <h3>{totalProducts} models</h3>
           <Filter />
@@ -26,7 +34,7 @@ export const Catalog: React.FC<Props> = ({ products, totalProducts }) => {
             ))}
           </div>
         </div>
-      ) : (
+      ) : !isLoading ? (
         <section style={{ display: 'flex', flexDirection: 'column' }}>
           <div className={styles.animation}>
             <LottieAnimation animationData={animationData} />
@@ -40,7 +48,7 @@ export const Catalog: React.FC<Props> = ({ products, totalProducts }) => {
             Try again
           </Link>
         </section>
-      )}
+      ) : null}
     </section>
   );
 };
