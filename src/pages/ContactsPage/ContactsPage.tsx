4d019c2f81a5dbfa5dc382dd2b50e5e317a7ce01
Yaroslav Horodynskyi <gorodinskiyyar@gmail.com>
2024-05-23 11:27:02 +0300

    add contacts and right phage

---
+++ src/pages/ContactsPage/ContactsPage.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styles from './contactsPage.module.scss';
+import { people } from './ourTeam';
+
+export const ContactsPage = () => {
+
+  const handleCardClick = (link: string | URL | undefined) => {
+    window.open(link, '_blank', 'noopener,noreferrer');
+  };
+
+  return (
+    <div className={styles.contactsPage}>
+      {people.map(person => (
+        <a
+          key={person.id}
+          className={styles.card}
+          href={person.link}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={person.image} alt={person.name} />
+          <h2 className={styles.cardTitle}>{person.name}</h2>
+          <p className={styles.cardText}>{person.description}</p>
+        </a>
+      ))}
+    </div>
+  );
+};
